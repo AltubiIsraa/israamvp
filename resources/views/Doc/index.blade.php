030c3dfa2a7c233d0bd8d69de1527d3b193fd3ca
israa <israaaltubi@gmail.com>
2022-01-25 23:54:05 +0400

    table

--- resources/views/Doc/index.blade.php
+++ resources/views/Doc/index.blade.php
@@ -1,11 +1,33 @@
+<head>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
 <div>
+
     <table>
-        <tr></tr>
-    </table>
+    <style>
+    table,
+    th,
+    td {
+      border: 1px solid black;
+    }
+  </style>
+        <tr>
+        <th>title</th>
+        <th>Description</th>
+        <th>subject</th>
+        <th>grade</th>
+
+        </tr>
+
     @foreach($docs as $doc)
-    <h1>{{$doc->title}}</h1>
-    <h1>{{$doc->Desc}}</h1>
-    <h1>{{$doc->subject}}</h1>
-    <h1>{{$doc->grade}}</h1>
+    <tr>
+    
+        <td>{{$doc->title}}</td>
+        <td>{{$doc->Desc}}</td>
+        <td>{{$doc->subject}}</td>
+        <td>{{$doc->grade}}</td>
+    </tr>
     @endforeach
+</table>
 </div>
